2c47ed14fee7a71b5ba52e8c5148707cd94cc5aa
SandeepKaur84 <kaur190sandeep@gmail.com>
2026-07-20 18:01:40 +0530

    Graphs in Java

---
+++ Graphs/Example.java
@@ -0,0 +1,30 @@
+package Graphs;
+
+import java.util.ArrayList;
+
+public class Example {
+    public static void main(String[] args) {
+        //0 -> 1 , 2
+        //1 -> 0 , 3
+        //2 -> 0 , 3
+        //3 -> 1 , 2
+        // [[1,2],[0,3],[0,3],[1,2]]
+
+        int vertices = 4;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i = 0 ; i < vertices ; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(0).add(1);
+        graph.get(0).add(2);
+        graph.get(1).add(0);
+        graph.get(1).add(3);
+        graph.get(2).add(0);
+        graph.get(2).add(3);
+        graph.get(3).add(1);
+        graph.get(3).add(2);
+
+        System.out.println(graph);
+    }
+}
